5976bc83943581a6663f6378a1d0568e395e3588
Phelix <phelixmbani@gmail.com>
2024-10-08 13:10:28 +0300

    fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', function(e){
+    
+    e.preventDefault()
+    let formContent = e.target.searchByID.value;
+    console.log(formContent)
+
+    fetch(`http://localhost:3000/movies/${formContent}`)
+    .then((response)=> response.json())
+    .then((data)=> {
+        const movieTitle = document.querySelector("section#movieDetails h4")
+        const movieSummary = document.querySelector("section#movieDetails p")
+
+            movieTitle.textContent = data.title
+            movieSummary.textContent = data.summary
+            
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
